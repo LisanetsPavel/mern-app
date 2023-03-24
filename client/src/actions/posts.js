fd48a110aaa579eea12dec796223499e2837f87f
p.lysanets <p.lysanets@aimprosoft.com>
2023-03-24 14:47:16 +0200

    first commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,77 @@
+import * as api from '../api'
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from "../constants/actionTypes";
+
+export const getPosts = () => async (dispatch) => {
+
+    try {
+        const { data } = await api.fetchPosts();
+        const action = {
+            type: FETCH_ALL,
+            payload: data
+        }
+
+        dispatch(action);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const createPost = (newPost) => async (dispatch) => {
+
+    try {
+        const { data } = await api.createPost(newPost);
+        const action = {
+            type: CREATE,
+            payload: data
+        }
+
+        dispatch(action);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+
+    try {
+        const { data } = await api.updatePost(id, post);
+        const action = {
+            type: UPDATE,
+            payload: data
+        }
+
+        dispatch(action);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+
+    try {
+        await api.deletePost(id);
+        const action = {
+            type: DELETE,
+            payload: id
+        }
+
+        dispatch(action);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+
+    try {
+        const { data } = await api.likePost(id);
+        const action = {
+            type: LIKE,
+            payload: data
+        }
+
+        dispatch(action);
+    } catch(error) {
+        console.log(error);
+    }
+}
